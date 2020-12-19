46c27c27c0fa5027d59671bb97a3b961a190c3a8
Notnox <74467549+Notnox@users.noreply.github.com>
2020-12-19 07:50:40 -0300

    responsivo completo

--- colegio/src/estrutura/Menu.jsx
+++ colegio/src/estrutura/Menu.jsx
@@ -3,26 +3,29 @@ import React from 'react'
 
 export default props => {
     return (
-        <header className='cabecalho'>
-            <div className='logoMarca'>
-                <div className='logo'></div>
-                <p className='empresa'>Colégio Amorinha</p>
-            </div>
-            <ul className='opLista'>
-                <li className='op'>
-                    <div className="busca">
-                        <input type="checkbox" id='check'/>
-                        <input type="text" id='txtBusca'/>
-                        <label htmlFor="check"><div className="lupa"></div></label>
-                    </div>
-                </li>
-                <li className='op'>
-                    <div className="pag">
-                        <p className='cadastro'>Cadastro</p>
-                        <p className='cadastro'>Alunos</p>
-                    </div>
-                </li>
-            </ul>
-        </header>
+        <main>
+            <header className='cabecalho'>
+                <div className='logoMarca'>
+                    <div className='logo'></div>
+                    <p className='empresa'>Colégio Amorinha</p>
+                </div>
+                <ul className='opLista'>
+                    <li className='op'>
+                        <div className="busca">
+                            <input type="checkbox" id='check'/>
+                            <input type="text" id='txtBusca'/>
+                            <label htmlFor="check"><div className="lupa"></div></label>
+                        </div>
+                    </li>
+                    <li className='op'>
+                        <div className="pag">
+                            <p className='cadastro'>Cadastro</p>
+                            <p className='cadastro'>Alunos</p>
+                        </div>
+                    </li>
+                </ul>
+            </header>
+            <p className='semSuporte'>Sem suporte para esse tamanho de tela</p>
+        </main>
     )
 }
